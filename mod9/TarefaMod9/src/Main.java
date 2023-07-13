f24f102a99e02a345d5694b766615f5393a81ddc
maxfideles <maxfideles24@gmail.com>
2023-07-13 14:45:46 +0200

    Module 9 Algorithm Exercise

--- mod9/TarefaMod9/src/Main.java
+++ mod9/TarefaMod9/src/Main.java
@@ -5,10 +5,8 @@ public class Main {
         long num1 = 777777777777L;
         System.out.println(num1);
         //Realizando o casting
-        int num2 = (int) num1;
-        //Convertendo para o tipo wrapper
-        Integer num3 = (Integer) num2;
-        System.out.println(num3);
+        Long num2 = (Long) num1;
+        System.out.println(num2);
 
     }
 }
