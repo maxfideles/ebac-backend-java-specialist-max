8d3d90fa94f6b08086a432e8d3c8ea98966ff369
maxfideles <maxfideles24@gmail.com>
2023-08-01 14:47:12 +0200

    Design Pattern Facade

--- mod16/EgFacade/src/Habitation.java
+++ mod16/EgFacade/src/Habitation.java
@@ -1,17 +1,17 @@
 public abstract class Habitation {
 
      private String type;
-     private String Address;
+     private String address;
      private int numRooms;
 
      abstract void summary();
 
     public String getAddress() {
-        return Address;
+        return address;
     }
 
     public void setAddress(String address) {
-        Address = address;
+        this.address = address;
     }
 
     public int getNumRooms() {
